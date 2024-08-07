b35d9bb5fb608277f88dd43fe057f4f68de423e9
Jakirul Islam <86887501+jakiruli@users.noreply.github.com>
2024-08-07 22:30:02 +0600

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,38 @@
+
+
+package product;
+
+/**
+ *
+ * @author mdjak
+ */
+public class Product {
+
+    public static void main(String[] args) {
+        // declareation
+       int id;
+       String title;
+       String price;
+       String description;
+       String Category;
+       
+       // value added
+       
+       id = 101;
+       title = "iphone15";
+       price = "1895 euros";
+       description = "perfect product with best image quality";
+       Category = "phone";
+       
+       // print out
+       
+        System.out.println("Product Id: "+id);
+        System.out.println("Price :"+title);
+        System.out.println("Description :"+description);
+        System.out.println("price :"+price);
+        System.out.println("Categiry :"+Category);
+       
+       
+       
+    }
+}
