61ae252b3b3dd424692ec380e0af51a3c14e05f0
Jakirul Islam <86887501+jakiruli@users.noreply.github.com>
2024-08-07 22:34:10 +0600

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,28 @@
+
+package datatype;
+
+/**
+ *
+ * @author mdjakirul islam
+ */
+public class DataType {
+
+    public static void main(String[] args) {
+        
+       boolean b= true;
+       char c ='a';
+       short s =3204;
+       int i = 126655;
+       float f = 10.2f;
+       double d = 10.2;
+       
+        System.out.println("boolean b"+b);
+        System.out.println("char c ="+c);
+        System.out.println("short ="+s);
+        System.out.println("int ="+i);
+        System.out.println("float f = "+f);
+        System.out.println("double = "+d);
+    }
+             
+    
+}
