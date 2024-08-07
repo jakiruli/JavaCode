815aa43fee0c27200ea4c7c0b0a5f27fb61732c0
Jakirul Islam <86887501+jakiruli@users.noreply.github.com>
2024-08-07 22:33:30 +0600

    Add files via upload

---
+++ ExecutionSteps.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package executionsteps;
+
+/**
+ *
+ * @author mdjak
+ */
+public class ExecutionSteps {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
